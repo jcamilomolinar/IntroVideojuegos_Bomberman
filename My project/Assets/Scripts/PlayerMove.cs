52cd51a394ab98b5e6eee6730cf2a8433fbe25a5
Juan Camilo Molina <jmolinaro@unal.edu.co>
2023-06-12 18:29:16 -0500

    movement fixed

--- My project/Assets/Scripts/PlayerMove.cs
+++ My project/Assets/Scripts/PlayerMove.cs
@@ -2,28 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimation : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
 
     [SerializeField]
-    public float velocidad = 5f;
+    public float velocidad;
     private Rigidbody2D rb;
     private Animator animator;
 
     void Start()
     {
-        animator = GetComponent<Animator>();
-        rb = GetComponentInChildren<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {   
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
-
         Vector2 movement = new Vector2(moveX, moveY) * velocidad;
         rb.velocity = movement;
-    
+
         if (moveX > 0)
         {
             animator.SetBool("Right", true);
