9315b838ee9802049236429bfcb286e1087cf8cd
Juan Camilo Molina <jmolinaro@unal.edu.co>
2023-06-12 20:18:37 -0500

    some improves in movement

--- My project/Assets/Scripts/PlayerMove.cs
+++ My project/Assets/Scripts/PlayerMove.cs
@@ -29,6 +29,7 @@ public class PlayerMove : MonoBehaviour
             animator.SetBool("Left", false);
             animator.SetBool("Up", false);
             animator.SetBool("Down", false);
+            animator.SetLayerWeight(1, 1);
         } 
         else if (moveX < 0)
         {
@@ -36,6 +37,7 @@ public class PlayerMove : MonoBehaviour
             animator.SetBool("Left", true);
             animator.SetBool("Up", false);
             animator.SetBool("Down", false);
+            animator.SetLayerWeight(1, 1);
         }
         else if (moveY > 0)
         {
@@ -43,6 +45,7 @@ public class PlayerMove : MonoBehaviour
             animator.SetBool("Left", false);
             animator.SetBool("Up", true);
             animator.SetBool("Down", false);
+            animator.SetLayerWeight(1, 1);
         }
         else if (moveY < 0)
         {
@@ -50,6 +53,11 @@ public class PlayerMove : MonoBehaviour
             animator.SetBool("Left", false);
             animator.SetBool("Up", false);
             animator.SetBool("Down", true);
+            animator.SetLayerWeight(1, 1);
+        }
+        else if (moveY == 0 && moveX == 0)
+        {
+            animator.SetLayerWeight(1, 0);
         }
     }
 
