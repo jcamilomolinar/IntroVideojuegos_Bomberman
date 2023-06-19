e13c194f2472d7267a8f162a1c3fb5fcede92efa
Juan Camilo Molina <jmolinaro@unal.edu.co>
2023-06-19 00:28:38 -0500

    Update PlayerMove.cs

--- My project/Assets/Scripts/PlayerMove.cs
+++ My project/Assets/Scripts/PlayerMove.cs
@@ -8,8 +8,10 @@ public class PlayerMove : MonoBehaviour, IDamageable
     [SerializeField]
     public float speed = 3;
     public float DisappearDelay;
+    public bool enableInput = true;
     private Rigidbody2D rb;
     private Animator animator;
+
     [field:SerializeField]
     public int TotalHealthPoints { get; private set; }
     public int HealthPoints { get; private set; }
@@ -23,48 +25,50 @@ public class PlayerMove : MonoBehaviour, IDamageable
 
     void Update()
     {   
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveX, moveY) * speed;
-        rb.velocity = movement;
+        if (enableInput)
+        {
+            float moveX = Input.GetAxis("Horizontal");
+            float moveY = Input.GetAxis("Vertical");
+            Vector2 movement = new Vector2(moveX, moveY) * speed;
+            rb.velocity = movement;
 
-        if (moveX > 0)
-        {
-            animator.SetBool("Right", true);
-            animator.SetBool("Left", false);
-            animator.SetBool("Up", false);
-            animator.SetBool("Down", false);
-            animator.SetLayerWeight(1, 1);
-        } 
-        else if (moveX < 0)
-        {
-            animator.SetBool("Right", false);
-            animator.SetBool("Left", true);
-            animator.SetBool("Up", false);
-            animator.SetBool("Down", false);
-            animator.SetLayerWeight(1, 1);
+            if (moveX > 0)
+            {
+                animator.SetBool("Right", true);
+                animator.SetBool("Left", false);
+                animator.SetBool("Up", false);
+                animator.SetBool("Down", false);
+                animator.SetLayerWeight(1, 1);
+            } 
+            else if (moveX < 0)
+            {
+                animator.SetBool("Right", false);
+                animator.SetBool("Left", true);
+                animator.SetBool("Up", false);
+                animator.SetBool("Down", false);
+                animator.SetLayerWeight(1, 1);
+            }
+            else if (moveY > 0)
+            {
+                animator.SetBool("Right", false);
+                animator.SetBool("Left", false);
+                animator.SetBool("Up", true);
+                animator.SetBool("Down", false);
+                animator.SetLayerWeight(1, 1);
+            }
+            else if (moveY < 0)
+            {
+                animator.SetBool("Right", false);
+                animator.SetBool("Left", false);
+                animator.SetBool("Up", false);
+                animator.SetBool("Down", true);
+                animator.SetLayerWeight(1, 1);
+            }
+            else if (moveY == 0 && moveX == 0)
+            {
+                animator.SetLayerWeight(1, 0);
+            }
         }
-        else if (moveY > 0)
-        {
-            animator.SetBool("Right", false);
-            animator.SetBool("Left", false);
-            animator.SetBool("Up", true);
-            animator.SetBool("Down", false);
-            animator.SetLayerWeight(1, 1);
-        }
-        else if (moveY < 0)
-        {
-            animator.SetBool("Right", false);
-            animator.SetBool("Left", false);
-            animator.SetBool("Up", false);
-            animator.SetBool("Down", true);
-            animator.SetLayerWeight(1, 1);
-        }
-        else if (moveY == 0 && moveX == 0)
-        {
-            animator.SetLayerWeight(1, 0);
-        }
-
     }
 
     public void TakeHit()
@@ -88,6 +92,7 @@ public class PlayerMove : MonoBehaviour, IDamageable
     {   
         if (collision.gameObject.CompareTag("Explosion"))
         {
+            enableInput = false;
             TakeHit();
         }
     }
