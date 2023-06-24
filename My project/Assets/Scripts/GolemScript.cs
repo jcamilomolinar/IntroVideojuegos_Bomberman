21ecbf73368aab086483e7f43d46e3c594ca1fca
Davidcito12 <dlopezza@unal.edu.co>
2023-06-23 22:50:18 -0500

    Updated stuff

--- My project/Assets/Scripts/GolemScript.cs
+++ My project/Assets/Scripts/GolemScript.cs
@@ -9,49 +9,74 @@ public class GolemScript : MonoBehaviour, IDamageable
     public float DisappearDelay;
     private Rigidbody2D rb;
     private Animator animator;
+    private Vector2 previousPosition;
 
     [field:SerializeField]
     public int TotalHealthPoints { get; private set; }
     public int HealthPoints { get; private set; }
 
+
     void Start()
     {
         animator = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponentInParent<Rigidbody2D>();
         HealthPoints = TotalHealthPoints;
+        previousPosition = transform.position;
     }
 
     void Update()
-    {   
-        float randomValue = Random.value;
-        if (randomValue < 0.25)
+    {
+        Vector2 velocity = rb.velocity;
+        Vector2 currentPosition = transform.position;
+        Vector2 difference = currentPosition - previousPosition;
+
+        if (difference.magnitude > 0.01f)
+        {
+            if (Mathf.Abs(difference.x) > Mathf.Abs(difference.y))
             {
-                animator.SetBool("Right", true);
-                animator.SetBool("Left", false);
-                animator.SetBool("Up", false);
-                animator.SetBool("Down", false);
-            }
-            else if (randomValue < 0.5)
-            {
-                animator.SetBool("Right", false);
-                animator.SetBool("Left", true);
-                animator.SetBool("Up", false);
-                animator.SetBool("Down", false);
-            }
-            else if (randomValue < 0.75)
-            {
-                animator.SetBool("Right", false);
-                animator.SetBool("Left", false);
-                animator.SetBool("Up", true);
-                animator.SetBool("Down", false);
+                // El objeto se está moviendo principalmente en el eje horizontal (izquierda o derecha)
+                if (difference.x > 0)
+                {
+                    animator.SetBool("Right", true);
+                    animator.SetBool("Left", false);
+                    animator.SetBool("Up", false);
+                    animator.SetBool("Down", false);
+                }
+                else
+                {
+                    animator.SetBool("Right", false);
+                    animator.SetBool("Left", true);
+                    animator.SetBool("Up", false);
+                    animator.SetBool("Down", false);
+                }
             }
             else
             {
-                animator.SetBool("Right", false);
-                animator.SetBool("Left", false);
-                animator.SetBool("Up", false);
-                animator.SetBool("Down", true);
+                // El objeto se está moviendo principalmente en el eje vertical (arriba o abajo)
+                if (difference.y > 0)
+                {
+                    animator.SetBool("Right", false);
+                    animator.SetBool("Left", false);
+                    animator.SetBool("Up", true);
+                    animator.SetBool("Down", false);
+                }
+                else
+                {
+                    animator.SetBool("Right", false);
+                    animator.SetBool("Left", false);
+                    animator.SetBool("Up", false);
+                    animator.SetBool("Down", true);
+                }
             }
+        }
+        else {
+            animator.SetBool("Right", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+        }
+
+        previousPosition = currentPosition;
     }
 
     public void TakeHit()
@@ -62,11 +87,13 @@ public class GolemScript : MonoBehaviour, IDamageable
             speed = 0;
             StartCoroutine(DisappearAfterDelay(DisappearDelay));
         }
+        
     }
 
     IEnumerator DisappearAfterDelay(float Delay)
     {
         yield return new WaitForSeconds(Delay);
+        Destroy(transform.parent.gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -77,3 +104,4 @@ public class GolemScript : MonoBehaviour, IDamageable
         }
     }
 }
+
