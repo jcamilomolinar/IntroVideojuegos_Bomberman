89f9fb349b02c73ba5c025a39e182ec03ff2f2db
Juan Camilo Molina <jmolinaro@unal.edu.co>
2023-06-23 20:16:06 -0500

    golem animation

---
+++ My project/Assets/Scripts/GolemScript.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemScript : MonoBehaviour, IDamageable
+{
+    [SerializeField]
+    public float speed = 1;
+    public float DisappearDelay;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    [field:SerializeField]
+    public int TotalHealthPoints { get; private set; }
+    public int HealthPoints { get; private set; }
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        HealthPoints = TotalHealthPoints;
+    }
+
+    void Update()
+    {   
+        float randomValue = Random.value;
+        if (randomValue < 0.25)
+            {
+                animator.SetBool("Right", true);
+                animator.SetBool("Left", false);
+                animator.SetBool("Up", false);
+                animator.SetBool("Down", false);
+            }
+            else if (randomValue < 0.5)
+            {
+                animator.SetBool("Right", false);
+                animator.SetBool("Left", true);
+                animator.SetBool("Up", false);
+                animator.SetBool("Down", false);
+            }
+            else if (randomValue < 0.75)
+            {
+                animator.SetBool("Right", false);
+                animator.SetBool("Left", false);
+                animator.SetBool("Up", true);
+                animator.SetBool("Down", false);
+            }
+            else
+            {
+                animator.SetBool("Right", false);
+                animator.SetBool("Left", false);
+                animator.SetBool("Up", false);
+                animator.SetBool("Down", true);
+            }
+    }
+
+    public void TakeHit()
+    {
+        HealthPoints--;
+        if(HealthPoints <= 0){
+            animator.SetBool("Death", true);
+            speed = 0;
+            StartCoroutine(DisappearAfterDelay(DisappearDelay));
+        }
+    }
+
+    IEnumerator DisappearAfterDelay(float Delay)
+    {
+        yield return new WaitForSeconds(Delay);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {   
+        if (collision.gameObject.CompareTag("Explosion"))
+        {
+            TakeHit();
+        }
+    }
+}
